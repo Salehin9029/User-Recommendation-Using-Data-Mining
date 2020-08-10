eebfa45e94cfc5fb62efa171985975e830731065
Salehin Rahman Khan <khan1fm@uwindsor.ca>
2020-08-10 14:54:50 -0400

    Fully Updated
    Done with the recommender settings

--- Datamining/Recommender.py
+++ Datamining/Recommender.py
@@ -4,6 +4,41 @@ from surprise import Dataset
 from surprise import Reader
 from collections import defaultdict
 from surprise.model_selection import cross_validate
+import databaseCon
+
+con = databaseCon.Database()
+
+loadedData = con.selectAll()
+
+userGroupId = loadedData[0]
+ingredientId = loadedData[1]
+ratings = loadedData[2]  # ALL MUST BE IN SAME LENGTH OR ERROR WILL COME
 
 
+def do_predict():
+    ratings_dic = {"userId": userGroupId,
+                   "itemId": ingredientId,
+                   "rating": ratings}
+    df = pd.DataFrame(ratings_dic)
+    reader = Reader(rating_scale=(1, 4))
+    data = Dataset.load_from_df(df[['userId', 'itemId', 'rating']], reader)
+    trainset = data.build_full_trainset()
+    algo = SVD()
+    algo.fit(trainset)
+    testset = trainset.build_anti_testset()
+    predictions = algo.test(testset)
+    cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    get_top_n(predictions)
 
+
+def get_top_n(predictions, n=20):
+    top_n = defaultdict(list)
+
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+    print(top_n)
+    for uid, user_ratings, in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse= True)
+        top_n[uid] = user_ratings[:n]
+
+do_predict()
--- Datamining/databaseCon.py
+++ Datamining/databaseCon.py
@@ -10,10 +10,30 @@ class Database:
   database="datamining",               #database name
     auth_plugin = 'mysql_native_password', use_pure= False)  # change here if you need any changes
         self.cursor = self.connection.cursor()
-
+                # REMEMBER THIS. GOT STUCK HERE
     def insert(self, query):
         try:
             self.cursor.execute(query)
             self.connection.commit()
         except:
             self.connection.rollback()
+
+    def selectAll(self):
+        data = []
+        users = []
+        items = []
+        ratings = []
+
+        self.cursor.execute("SELECT rating, users_idUsers, Recipe_idRecipe from rating;")
+        rows = self.cursor.fetchall()
+
+        for r in rows:
+            ratings.append(r[0])
+            users.append(r[1])
+            items.append(r[2])
+
+        data.append(users)
+        data.append(items)
+        data.append(ratings)
+
+        return data
